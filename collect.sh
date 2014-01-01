16cb1247e57128344d68a2cb20c59789b890c85f
A.J. Rouvoet <a.j.rouvoet@gmail.com>
2014-01-01 16:40:38 +0100

    improved output of data collection script

--- collect.sh
+++ collect.sh
@@ -2,13 +2,15 @@
 OUTDIR=./data/
 SUITE=$1
 REF=`git rev-parse HEAD`
-OPTS="-rnd-freq=1
--rnd-freq=0.02 -activity-nl-freq=1
+OPTS="-rnd-freq=0.02 -activity-nl-freq=1
 -rnd-freq=0.02 -sym-count-freq=1"
 
 echo "$OPTS" | while read opt
 do
+	echo "-----------------------------------------------------------------------------------------"
 	echo ">> running with options: $opt"
+	echo "-----------------------------------------------------------------------------------------"
+	echo ""
 
 	find minisat/cnf\ test\ files/$SUITE -regex ".*\.cnf$" | while read f
 	do
@@ -16,11 +18,13 @@ do
 		name=`basename "$f"`
 		suite=`basename "$(dirname $f)"`
 		outpath="$OUTDIR/$REF/$optpath/$suite/$name.log"
+		outdir=`dirname "$outpath"`
 
-		mkdir -p `dirname "$outpath"`
+		mkdir -p "$outdir"
 
 		echo ">> Running $name from suite $suite ($EXC "$f")"
-		./minisat/build/release/bin/minisat_core "$f" > $outpath
+		./minisat/build/release/bin/minisat_core "${f}" > "${outpath}"
 		echo ">> Done (output in: $outpath)"
+		echo ""
 	done
 done
