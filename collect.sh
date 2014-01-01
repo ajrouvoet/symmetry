f9b860d28f0bda5a9ef3edff32c99427fa142041
A.J. Rouvoet <a.j.rouvoet@gmail.com>
2014-01-01 16:33:34 +0100

    Updated test script to run with multiple options

--- collect.sh
+++ collect.sh
@@ -1,18 +1,26 @@
 #!/bin/bash
 OUTDIR=./data/
 SUITE=$1
-BRANCH=`git branch | sed -n '/\* /s///p'`
 REF=`git rev-parse HEAD`
+OPTS="-rnd-freq=1
+-rnd-freq=0.02 -activity-nl-freq=1
+-rnd-freq=0.02 -sym-count-freq=1"
 
-find minisat/cnf\ test\ files/$SUITE -regex ".*\.cnf$" | while read f
+echo "$OPTS" | while read opt
 do
-	name=`basename "$f"`
-	suite=`basename "$(dirname $f)"`
-	outpath="$OUTDIR/$BRANCH/$REF/$suite/$name.log"
+	echo ">> running with options: $opt"
 
-	mkdir -p `dirname "$outpath"`
+	find minisat/cnf\ test\ files/$SUITE -regex ".*\.cnf$" | while read f
+	do
+		optpath=`echo $opt | sed "s/ :;/_/g"`
+		name=`basename "$f"`
+		suite=`basename "$(dirname $f)"`
+		outpath="$OUTDIR/$REF/$optpath/$suite/$name.log"
 
-	echo ">> Running $name from suite $suite ($EXC "$f")"
-	./minisat/build/release/bin/minisat_core "$f" > $outpath
-	echo ">> Done (output in: $outpath)"
+		mkdir -p `dirname "$outpath"`
+
+		echo ">> Running $name from suite $suite ($EXC "$f")"
+		./minisat/build/release/bin/minisat_core "$f" > $outpath
+		echo ">> Done (output in: $outpath)"
+	done
 done
